134acd48e49baa0a88ff7e03d80f7ad4259d9a01
Sumit Kandari <sumitkandari69@gmail.com>
2021-07-16 10:03:55 +0530

    first_commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Material(
+
+
+          child:Center(
+            child: Container(
+              child: Text("Day 1"),
+            ),
+          ) ,
+        ),
+    );
+   }
+}
+      
